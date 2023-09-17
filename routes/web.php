cff687489bb487e8585330f54b608ad19f85931d
Hatem <hatem.soliman333@gmail.com>
2023-09-17 21:53:53 +0300

    adding category routes and controllers
    - added the category controller to handle indexing main categories and
      retrieving the subcategories of a parent category and if not found it
      will create them

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/subcategories/{parentCategoryId}', [CategoryController::class, 'getSubcategories'])->name('getsubcategries');
